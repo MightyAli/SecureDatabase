1fdf87f2ad871add6715f24adecfcddb4c6c0497
Ali Al-Ramadan <55710790+MightyAli@users.noreply.github.com>
2022-11-25 15:17:17 -0500

    Update SQLQuery3.sql

--- Final/SQLQuery3.sql
+++ Final/SQLQuery3.sql
@@ -1,26 +1,10 @@
-
-
-CREATE TABLE alithe2 (
-a int not null primary key,
-);
-
-CREATE TABLE alithe (
-b int,
-foreign key(b) references alithe2(a) on update cascade on delete cascade,
-);
-
-insert into alithe(b) values (4);
-insert into alithe2(a) values (4);
-select a from alithe2 where a = 4;
-
-
-
 CREATE TABLE Customer(
 Customer_ID INT PRIMARY KEY,
 Fname VARCHAR(20) NOT NULL,
 Lname VARCHAR(20) NOT NULL,
 Mobile_Num INT,
-Password VARCHAR(10) NOT NULL
+Password VARCHAR(66) NOT NULL,
+Salt VARCHAR(18) NOT NULL
 );
 
 CREATE TABLE CD(
@@ -37,8 +21,3 @@ CD_ID INT FOREIGN KEY REFERENCES CD(CD_ID),
 Order_Quantity INT,
 Order_date datetime
 );
-
-SELECT CD.CD_ID, Order_Quantity, Order_date, Unit_Price ,Movie_Name
-FROM CD  , OrderT , Customer
-WHERE CD.CD_ID = OrderT.CD_ID AND Customer.Customer_ID = OrderT.Customer_ID
-ORDER BY Customer.Customer_ID
